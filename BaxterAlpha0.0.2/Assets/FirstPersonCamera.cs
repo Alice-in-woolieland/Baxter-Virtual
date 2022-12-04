c7a2a8623a855c62cf403327b509cedcd39d87e1
Alice-in-woolieland <119767457+Alice-in-woolieland@users.noreply.github.com>
2022-12-04 01:05:16 -0800

    Baxter 0.0.2
    Adding project to repo

---
+++ BaxterAlpha0.0.2/Assets/FirstPersonCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    // Variables
+    public Transform player;
+    public float mouseSensitivity = 2f;
+    float cameraVerticalRotation = 0;
+
+    void Start()
+    {
+        // Lock and Hide the Cursor
+    }
+
+    void Update()
+    {
+        // Collect Mouse Input
+
+        float inputX = Input.GetAxis("Mouse X")*mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y")*mouseSensitivity;
+
+        // Rotate the Camera around its local X axis
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        // Rotate the Player Object and the Camera around its Y axis
+    }
+}
